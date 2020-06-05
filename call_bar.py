cf25758fd5760e84b56d9578f851f204fcd26f8f
Jhongesell <unmsm182@gmail.com>
2020-06-05 17:51:49 -0500

    agregando cajas rojas

--- call_bar.py
+++ call_bar.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 #from power_bar import PowerBar
-from power_bar_v02 import PowerBar
+#from power_bar_v02 import PowerBar
+from power_bar_v03 import PowerBar
 
 
 
