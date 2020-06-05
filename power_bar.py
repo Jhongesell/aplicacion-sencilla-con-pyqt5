694f7641853f08471d20337c3199b98840cf138c
Jhongesell <unmsm182@gmail.com>
2020-06-05 03:47:44 -0500

    agregando contenido en pantalla en power_Bar

--- power_bar.py
+++ power_bar.py
@@ -2,15 +2,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 
 class _Bar(QtWidgets.QWidget):
-
-    def paintEvent(self, e):
-        painter = QtGui.QPainter(self)
-        brush = QtGui.QBrush()
-        brush.setColor(QtGui.QColor('black'))
-        brush.setStyle(Qt.SolidPattern)
-        rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
-        painter.fillRect(rect, brush)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -19,7 +10,38 @@ class _Bar(QtWidgets.QWidget):
             QtWidgets.QSizePolicy.MinimumExpanding)
 
     def sizeHint(self):
-        return QtCore.QSize(300, 120)
+        return QtCore.QSize(40, 120)
+
+    def paintEvent(self, e):
+        painter = QtGui.QPainter(self)
+        
+        brush = QtGui.QBrush()
+        brush.setColor(QtGui.QColor('black'))
+        brush.setStyle(Qt.SolidPattern)
+        rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
+        painter.fillRect(rect, brush)
+
+        # Get current state
+        dial = self.parent()._dial
+        vmin, vmax = dial.minimum(), dial.maximum()
+        value = dial.value()
+
+        pen = painter.pen()
+        pen.setColor(QtGui.QColor('red'))
+        painter.setPen(pen)
+
+        font = painter.font()
+        font.setFamily('Times')
+        font.setPointSize(18)
+        painter.setFont(font)
+
+        painter.drawText(25, 25, "{}-->{}<--{}".format(vmin, value, vmax))
+        painter.end()
+
+    def _trigger_refresh(self):
+        self.update()
+
+        
 
 class PowerBar(QtWidgets.QWidget):
     """Custom Qt wIDGET TO SHOW A POWER BAR AND DIAL.
@@ -37,4 +59,4 @@ class PowerBar(QtWidgets.QWidget):
 
         self.setLayout(layout)
 
-        
+        self._dial.valueChanged.connect(self._bar._trigger_refresh)
